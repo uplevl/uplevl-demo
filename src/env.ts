6b826d2f5930fcc790cdd2ed9885758d4b9c4e60
Florian Müller <flomueller1981@gmail.com>
2025-09-15 21:50:35 +0200

    feat: integrate Sentry for error tracking and performance monitoring, add configuration for client and server environments

--- src/env.ts
+++ src/env.ts
@@ -25,11 +25,13 @@ export const env = createEnv({
 
   client: {
     NEXT_PUBLIC_URL: z.string().min(1),
+    NEXT_PUBLIC_SENTRY_DSN: z.string().min(1),
   },
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
     DATABASE_URL: process.env.DATABASE_URL,
     DB_MIGRATING: process.env.DB_MIGRATING,
     DB_SEEDING: process.env.DB_SEEDING,
